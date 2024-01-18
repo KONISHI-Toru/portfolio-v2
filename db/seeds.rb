64f29b72f3e430478b6033e2d3326a544a084704
KONISHI Toru <sting@nifty.com>
2024-01-18 19:17:44 +0900

    ユーザ認証機能追加。

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+admin_email = ENV.fetch("ADMIN_EMAIL", nil)
+admin_password = ENV.fetch("ADMIN_PASSWORD", nil)
+
+User.create(
+    email: admin_email,
+    password: admin_password
+) if admin_email.present? && admin_password.present?
